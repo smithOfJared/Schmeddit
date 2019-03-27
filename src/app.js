5690df4920a63268acfa8ba476c802026f0be35e
Jared Smith <j.danielsmith451@gmail.com>
2019-03-27 10:47:34 -0700

    fixing missing define server issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
-const appConfig = require("./config.main-congig.js");
+const appConfig = require("./config/main-config.js");
 
 routeConfig.init(app);
 appConfig.init();
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
+const server = http.createServer(app);
 
 app.set("port", port);
 
