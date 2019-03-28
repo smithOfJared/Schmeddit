0d453667fcc1299e97fe1675d56874bee7e67c2d
Jared Smith <j.danielsmith451@gmail.com>
2019-03-28 15:42:47 -0700

    adding 's' to baseScript(s)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const routeConfig = require("./config/route-config.js");
 const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
 
-routeConfig.init(app, express);
 appConfig.init(app, express);
+routeConfig.init(app);
 
 module.exports = app;
