b045663c2528221287620285c9f15e216afb5b16
Jared Smith <j.danielsmith451@gmail.com>
2019-04-01 16:28:06 -0700

    Revising checkoint4-bloccit

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,12 +3,21 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes: static", () => {
+  describe("GET /about", () => {
+    it("should return an About Us in the view", (done) => {
+      request.get(base.concat("about"),(err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us.");
+        done();
+      });
+    });
+  });
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-        //done();
+        done();
       });
     });
   });
