0baf3f1da078c2e8f60fa6d37bda23bae9e28d3a
Jared Smith <j.danielsmith451@gmail.com>
2019-04-02 13:40:35 -0700

    commit for checkpoint6 follow-along work--bloccit

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,17 +1,67 @@
 const Topic = require("./models").Topic;
 
 module.exports = {
-
-//#1
   getAllTopics(callback){
-    return Topic.all()
-
-//#2
+    return Topic.findAll()
     .then((topics) => {
       callback(null, topics);
     })
     .catch((err) => {
       callback(err);
     })
+  },
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  getTopic(id, callback){
+    return Topic.findById(id)
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  deleteTopic(id, callback){
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  updateTopic(id, updatedTopic, callback){
+    return Topic.findById(id)
+    .then((topic) => {
+      if(!topic){
+        return callback("Topic not found");
+      }
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
   }
+
+
 }
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -4,5 +4,12 @@ const router = express.Router();
 const topicController = require("../controllers/topicController")
 
 router.get("/topics", topicController.index);
+router.get("/topics/new", topicController.new);
+router.post("/topics/create", topicController.create);
+router.get("/topics/:id", topicController.show);
+router.post("/topics/:id/destroy", topicController.destroy);
+router.get("/topics/:id/edit", topicController.edit);
+router.post("/topics/:id/update", topicController.update);
+
 
 module.exports = router;
