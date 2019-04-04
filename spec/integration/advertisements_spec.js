81d211de432e44c1a11af55360ef2e83179f8c80
Jared Smith <j.danielsmith451@gmail.com>
2019-04-04 14:31:13 -0700

    adding advertisements view index file

--- spec/integration/advertisements_spec.js
+++ spec/integration/advertisements_spec.js
@@ -1,39 +1,40 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000//";
+const base = "http://localhost:3000/advertisements/";
 const sequelize = require("../../src/db/models/index").sequelize;
 //const Topic = require("../../src/db/models").Topic;
 const Advertisement = require("../../src/db/models").Advertisement;
 
 describe("routes : advertisements", () => {
 
-  beforeEach((done) => {
-    this.advertisement;
-    sequelize.sync({force: true}).then((res) => {
+    beforeEach((done) => {
+        this.advertisement;
+        sequelize.sync({ force: true }).then((res) => {
 
-      Advertisement.create({
-        title: "Doink-It!",
-        description: "Only babies don't have this toy!"
-      })
-      .then((advertisement) => {
-        this.advertisement = advertisement;
-        done();
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
+            Advertisement.create({
+                    title: "Doink-It!",
+                    description: "Only babies don't have this toy!"
+                })
+                .then((advertisement) => {
+                    this.advertisement = advertisement;
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+        });
     });
-  });
 
-  describe("GET / advertisements", () => {
+    describe("GET / advertisements", () => {
 
-    it("should show the latest and hottest thing you don't need", (done) => {
-      request.get(`${base}new`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("New Advertisement");
-        done();
-      });
+        it("should show the latest and hottest thing you don't need", (done) => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New Advertisement");
+                expect(body).toContain(this.advertisement.title);
+                done();
+            });
+        });
     });
-  });
 });
