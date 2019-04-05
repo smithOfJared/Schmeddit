cc59ef8fb614f54444a384eea79a576d857ccc49
Jared Smith <j.danielsmith451@gmail.com>
2019-04-05 07:30:52 -0700

    submission for checkpoint6-- bloccit

--- src/controllers/advertisementController.js
+++ src/controllers/advertisementController.js
@@ -1,72 +1,73 @@
 const advertisementQueries = require("../db/queries.advertisements.js");
 
 module.exports = {
-    index(req, res, next) {
-        advertisementQueries.getAllAdvertisements((err, advertisements) => {
-            if (err) {
-                res.direct(500, "static/index");
-            } else {
-                res.render("advertisements/index", { advertisements });
-            }
-        })
-    },
+  index(req, res, next){
+    advertisementQueries.getAllAdvertisements((err, advertisements) => {
+      if(err){
+        res.direct(500, "static/index");
+      }
+      else {
+        res.render("advertisements/index", {advertisements});
+      }
+    })
+  },
 
-    new(req, res, next) {
-        res.render("advertisements/new");
-    },
+  new(req, res, next){
+    res.render("advertisements/new");
+  },
 
-    create(req, res, next) {
-        let newAdvertisement = {
-            title: req.body.title,
-            description: req.body.description
-        };
-        advertisementQueries.addAddvertisement(newAdvertisement, (err, advertisement) => {
-            if (err) {
-                res.redirect(500, "/advertisements/new");
-            } else {
-                res.redirect(303, `/advertisements/${advertisement.id}`);
-            }
-        });
-    },
+  create(req, res, next){
+    let newAdvertisement = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    advertisementQueries.addAdvertisement(newAdvertisement, (err, advertisement) => {
+      if(err){
+        res.redirect(500, "/advertisements/new");
+      } else {
+        res.redirect(303, `/advertisements/${advertisement.id}`);
+      }
+    });
+  },
 
-    show(req, res, next) {
-        advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
-            if (err || advertisement == null) {
-                res.redirect(404, "/");
-            } else {
-                res.render("advertisements/show", { advertisement });
-            }
-        });
-    },
+  show(req, res, next){
+    advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+      if(err || advertisement == null){
+        res.redirect(404, "/");
+      } else {
+        res.render("advertisements/show", {advertisement});
+      }
+    });
+  },
 
-    destroy(req, res, next) {
-        advertisementQueries.deleteAdvertisement(req.params.id, (err, advertisement) => {
-            if (err) {
-                res.redirect(500, `/advertisements/${advertisement.id}`)
-            } else {
-                res.redirect(303, "/advertisements")
-            }
-        });
-    },
+  destroy(req, res, next){
+    advertisementQueries.deleteAdvertisement(req.params.id, (err, advertisement) => {
+      if(err){
+        res.redirect(500, `/advertisements/${advertisement.id}`)
+      } else {
+        res.redirect(303, "/advertisements")
+      }
+    });
+  },
 
-    edit(req, res, next) {
-        advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
-            if (err || advertisement == null) {
-                res.redirect(404, "/");
-            } else {
-                res.render("advertisements/edit", { advertisement });
-            }
-        });
-    },
+  edit(req, res, next){
+    advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+      if(err || advertisement == null){
+        res.redirect(404, "/");
+      } else {
+        res.render("advertisements/edit", {advertisement});
+      }
+    });
+  },
 
-    update(req, res, next) {
-        advertisementQueries.updateadvertisement(req.params.id, req.body, (err, advertisement) => {
-            if (err || advertisement == null) {
-                res.redirect(404, `/advertisements/${req.params.id}/edit`);
-            } else {
-                res.redirect(`/advertisements/${advertisement.id}`);
-            }
-        });
-    }
+  update(req, res, next){
+    advertisementQueries.updateAdvertisement(req.params.id, req.body, (err, advertisement) => {
+      if(err || advertisement == null){
+        res.redirect(404, `/advertisements/${req.params.id}/edit`);
+      } else {
+        res.redirect(`/advertisements/${advertisement.id}`);
+      }
+    });
+  }
 
 }
--- src/db/seeds/20190404190917-advertisements.js
+++ src/db/seeds/20190404190917-advertisements.js
@@ -2,7 +2,6 @@
 
 const faker = require("faker");
 
-//#2
  let advertisements = [];
 
  for(let i = 1 ; i <= 15 ; i++){
@@ -17,27 +16,10 @@ const faker = require("faker");
 //#3
  module.exports = {
    up: (queryInterface, Sequelize) => {
-     /*
-       Add altering commands here.
-       Return a promise to correctly handle asynchronicity.
-
-       Example:
-       return queryInterface.bulkInsert("Person", [{
-         name: "John Doe",
-         isBetaMember: false
-       }], {});
-     */
      return queryInterface.bulkInsert("Advertisements", advertisements, {});
    },
 
    down: (queryInterface, Sequelize) => {
-     /*
-       Add reverting commands here.
-       Return a promise to correctly handle asynchronicity.
-
-       Example:
-       return queryInterface.bulkDelete("Person", null, {});
-     */
      return queryInterface.bulkDelete("Advertisements", null, {});
    }
  };
