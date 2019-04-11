95dfdad2c95a1b510ca4b6717bbbd8b3b278eaff
Jared Smith <j.danielsmith451@gmail.com>
2019-04-11 09:15:18 -0700

    Commit for checkpoint9--bloccit follow-along work

---
+++ src/routes/posts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const postController = require("../controllers/postController")
+
+router.get("/topics/:topicId/posts/new", postController.new);
+router.get("/topics/:topicId/posts/:id", postController.show);
+router.post("/topics/:topicId/posts/create", postController.create);
+router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
+router.get("/topics/:topicId/posts/:id/edit", postController.edit);
+router.post("/topics/:topicId/posts/:id/update", postController.update);
+
+
+module.exports = router;
